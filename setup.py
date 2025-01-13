b19ebac36c12f2e4099f8855580fca8457d911cb
Iman Attary <121225353+ImanAttary@users.noreply.github.com>
2025-01-13 18:20:35 +0330

    update sdk version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zarinpal-py-sdk',               
-    version='0.2.2',                      
+    version='0.2.3',                      
     packages=find_packages(where="src/zarinpal-py-sdk"), 
     include_package_data=True,  
     py_modules=['zarinpal'],   
